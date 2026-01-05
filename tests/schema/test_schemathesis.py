8896303b6334c02bb39667a4afd0bfdb8da0bc28
Fadl <chaos@efqr.dev>
2026-01-05 15:14:22 +0100

    fix st test failure

--- tests/schema/test_schemathesis.py
+++ tests/schema/test_schemathesis.py
@@ -3,6 +3,7 @@ from urllib.parse import urlencode
 import schemathesis
 import pytest
 from schemathesis.core.transport import Response as SchemathesisResponse
+from dogesec_commons.identity.serializers import IdentitySerializer
 from dogesec_commons.wsgi import application as wsgi_app
 from rest_framework.response import Response as DRFResponse
 from schemathesis.specs.openapi.checks import (
@@ -10,6 +11,7 @@ from schemathesis.specs.openapi.checks import (
     positive_data_acceptance,
 )
 from schemathesis.config import GenerationConfig
+from hypothesis import strategies
 
 schema = schemathesis.openapi.from_wsgi(
     "/api/schema/?format=json",
@@ -17,6 +19,30 @@ schema = schemathesis.openapi.from_wsgi(
 )
 schema.config.base_url = "http://localhost:8005/"
 schema.config.generation = GenerationConfig(allow_x00=False)
+
+
+@pytest.fixture
+def identities(db):
+    identities = []
+    for name, stix_id in [
+        ("Test Identity 1", "identity--73faab8f-9a95-4417-a2db-c1a8b73c7029"),
+        ("Test Identity 2", "identity--90b66c75-8439-4fe2-8a0b-f2e6ff0d00b6"),
+    ]:
+        identity_s = IdentitySerializer(
+            data=dict(
+                id=stix_id,
+                name=name,
+                identity_class="organization",
+                sectors=["technology"],
+                created="2020-01-01T00:00:00.000Z",
+                modified="2020-01-01T00:00:00.000Z",
+            )
+        )
+        identity_s.is_valid(raise_exception=True)
+        identity = identity_s.save()
+        identity.refresh_from_db()
+        identities.append(identity)
+    yield identities
 
 
 @pytest.fixture(autouse=True)
@@ -64,8 +90,20 @@ def override_transport(monkeypatch, client):
 
 
 @pytest.mark.django_db(transaction=True)
+@schema.given(
+    identity_id=strategies.sampled_from(
+        [
+            "identity--73faab8f-9a95-4417-a2db-c1a8b73c7029",
+            "identity--90b66c75-8439-4fe2-8a0b-f2e6ff0d00b6",
+            "identity--89718333-15ec-4ffd-945e-831fe133f58c",
+        ]
+    )
+)
 @schema.parametrize()
-def test_api(case: schemathesis.Case):
+def test_api(case: schemathesis.Case, **kwargs):
+    for k, v in kwargs.items():
+        if k in case.path_parameters:
+            case.path_parameters[k] = v
     case.call_and_validate(
         excluded_checks=[negative_data_rejection, positive_data_acceptance]
     )

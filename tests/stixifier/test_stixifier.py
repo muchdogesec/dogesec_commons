134de2712c92297b10a88ebd302f2288bc8edbc4
Lullah <chaos@efqr.dev>
2026-03-12 12:08:11 +0100

    fix failing test

--- tests/stixifier/test_stixifier.py
+++ tests/stixifier/test_stixifier.py
@@ -171,6 +171,7 @@ def test_txt2stix(fake_file, fake_profile, settings):
             relationship_mode="full",
             ignore_extraction_boundary=True,
             ai_extract_if_no_incidence=False,
+            txt2stix_data=None,
         )
 
         assert processor.txt2stix_data == mock_run.return_value
@@ -224,7 +225,7 @@ def test_process(fake_file, fake_profile):
         patch.object(StixifyProcessor, "write_bundle"),
         patch.object(StixifyProcessor, "upload_to_arango") as mock_upload_to_arango,
     ):
-        assert processor.process() == mock_t2s.return_value.report.id
+        assert processor.process() == mock_t2s.return_value.report["id"]
         mock_f2t.assert_called_once()
         mock_t2s.assert_called_once()
         mock_upload_to_arango.assert_called_once()
